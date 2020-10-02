886b21c314ccea86fe39238b8b062a12c98c6ad7
AngeloTrotta85 <angelo@magilia.com>
2020-10-02 14:57:56 +0200

    ok

--- src/sidelink-eval.cpp
+++ src/sidelink-eval.cpp
@@ -6,10 +6,185 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>     // std::cout
+#include <fstream>      // std::ifstream
+#include <algorithm>    // std::find
+#include <vector>       // std::vector
+#include <list>       // std::list
+#include <stack>
+#include <map>       // std::list
+#include <cstdlib>
+#include <ctime>
+#include <random>
+#include <chrono>
+
+#include <boost/algorithm/string.hpp>
+
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+class InputParser{
+public:
+	InputParser (int &argc, char **argv){
+		for (int i=1; i < argc; ++i)
+			this->tokens.push_back(std::string(argv[i]));
+	}
+	const std::string& getCmdOption(const std::string &option) const{
+		std::vector<std::string>::const_iterator itr;
+		itr =  std::find(this->tokens.begin(), this->tokens.end(), option);
+		if (itr != this->tokens.end() && ++itr != this->tokens.end()){
+			return *itr;
+		}
+		static const std::string empty_string("");
+		return empty_string;
+	}
+	bool cmdOptionExists(const std::string &option) const{
+		return std::find(this->tokens.begin(), this->tokens.end(), option)
+		!= this->tokens.end();
+	}
+private:
+	std::vector <std::string> tokens;
+};
+
+class Arc {
+public:
+	int id;
+	int nodeStart;
+	int nodeEnd;
+	int txTime;
+	int channel;
+	int genPoI;
+	int genTime;
+};
+
+class Stat {
+public:
+	Stat(){
+		probRcv = -1;
+		delay = 0;
+	};
+public:
+	double probRcv;
+	double delay;
+};
+
+bool compare_packets (const Arc *first, const Arc *second) {
+	return (first->txTime < second->txTime);
+}
+
+int main(int argc, char **argv) {
+
+	map<pair<int, int>, list<Arc *>> arcMap;
+	map<int, list<Arc *>> arcMap_tx;
+
+	map<pair<int, int>, Stat *> risMap;
+
+	string fin = string("input.txt");
+	string fout = string("ris.txt");
+
+	InputParser input(argc, argv);
+
+	const std::string &in_string = input.getCmdOption("-fin");
+	if (!in_string.empty()) {
+		fin = in_string;
+	}
+
+	ifstream infile;
+	infile.open (fin, std::ifstream::in);
+	if (infile.is_open()){
+		int ididx = 0;
+		std::string line;
+
+		while (std::getline(infile, line)) {
+			std::string delimiter_field = ";";
+			std::string delimiter_eq = ":";
+
+		    cout << "Line: " << line << endl;
+
+		    vector<string> strs;
+		    boost::split(strs, line, boost::is_any_of(";"));
+
+		    Arc *a = new Arc();
+		    a->id = ididx++;
+
+		    int colIdx = 0;
+		    for (auto& var : strs) {
+		    	cout << var << endl;
+
+		    	vector<string> strs_var;
+		    	boost::split(strs_var, var, boost::is_any_of(":"));
+
+		    	for (auto& el : strs_var) {
+		    		cout << el << endl;
+		    	}
+
+		    	if (strs_var.size() == 2) {
+		    		if (strs_var[0].compare("U") == 0) {
+		    			if (colIdx == 0) {
+		    				a->nodeStart = stoi(strs_var[1]);
+		    			}
+		    			else {
+		    				a->nodeEnd = stoi(strs_var[1]);
+		    			}
+		    		}
+		    		else if (strs_var[0].compare("B") == 0) {
+		    			a->nodeEnd = stoi(strs_var[1]);
+		    		}
+		    		else if (strs_var[0].compare("TS") == 0) {
+		    			a->txTime = stoi(strs_var[1]);
+		    		}
+		    		else if (strs_var[0].compare("CH") == 0) {
+		    			a->channel = stoi(strs_var[1]);
+		    		}
+		    		else if (strs_var[0].compare("POI") == 0) {
+		    			a->genPoI = stoi(strs_var[1]);
+		    		}
+		    		else if (strs_var[0].compare("TX") == 0) {
+		    			a->genTime = stoi(strs_var[1]);
+		    		}
+		    	}
+
+		    	++colIdx;
+		    }
+
+		    //if (arcMap.count(a->gen) == 0) {
+		    //	arcMap[a->gen] = std::list<Arc *>();
+		    //}
+		    arcMap[make_pair(a->genPoI, a->genTime)].push_back(a);
+		    arcMap_tx[a->txTime].push_back(a);
+		}
+
+		infile.close();
+	}
+
+	//sort and print and check
+	for (auto& m : arcMap) {
+		int start = -1;
+		m.second.sort(compare_packets);
+		cout << "For packet generated at PoI " << m.first.first << " at time " << m.first.second << " we have: " << endl;
+		for (auto& l : m.second) {
+			cout << "   From " << l->nodeStart << " to " << l->nodeEnd << " using channel " << l->channel << " at time slot " << l->txTime << endl;
+
+			if ( ((start >= 0) && (start != l->nodeStart)) || (l->nodeStart == l->nodeEnd)) {
+				cerr << "Error in calculating the routing" << endl;
+				cerr << "   From " << l->nodeStart << " to " << l->nodeEnd << " using channel " << l->channel << " at time slot " << l->txTime << endl;
+			}
+			start = l->nodeEnd;
+		}
+		cout << endl;
+
+		risMap[m.first] = new Stat();
+	}
+
+	for (auto& m : arcMap) {
+		double totProb = 0;
+		double totDelay = 0;
+
+		risMap[m.first]->delay = totDelay;
+		risMap[m.first]->probRcv = totProb;
+	}
+
+	cout << endl << "End" << endl; // prints !!!Hello World!!!
+	return EXIT_SUCCESS;
 }
